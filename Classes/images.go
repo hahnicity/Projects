3cbe524158893aee0e926a38850c9f1e325105d2
hahnicity <grehm87@gmail.com>
2013-08-09 01:39:53 -0700

    Images Viewer Adding the images viewer project. Interesting thing is how many dependencies were required for this project. But they were nice to make

---
+++ Classes/images.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "flag"
+    "github.com/hahnicity/filetree"
+    "github.com/hahnicity/filterutils"
+    "os"
+    "os/exec"
+    "strings"
+)
+
+
+//var imageViewerCmd := "eog"
+const imageViewer string = "eog"
+
+// Gallery abstract class //
+type Gallery interface {
+    ShowImages(imagePath string)
+}
+
+// PNG class //
+type PNG struct {}
+
+func (p *PNG) ShowImages(imagePath string) {
+    /* Find all files in path with .png and shows them*/
+    files := findFiles(imagePath, ".bmp")
+    runCmd(files)
+}
+
+// BMP class //
+type BMP struct {}
+
+func (b *BMP) ShowImages(imagePath string) {
+    /*Find all files in path with .bmp and shows them*/
+    files := findFiles(imagePath, ".bmp")
+    runCmd(files)
+}
+
+//Helper functions
+func runCmd(files []string) {
+    /* Run the image viewer */
+    cmd := exec.Command(imageViewer, files...)
+    cmd.Run()
+}
+
+func findFiles(imagePath, fileType string) []string {
+    dir, err := filetree.GetDir(imagePath)
+    if err != nil {
+        panic(err)    
+    }
+    files, err := dir.GetFilePaths()
+    if err != nil {
+        panic(err)    
+    }
+    files = filterutils.Filter(
+        files, 
+        func(i int) bool { return strings.HasSuffix(files[i], fileType) },
+    )
+    return files
+}
+
+// main block
+func main() {
+    /*Show some images*/
+    var imageType string
+    var path string
+    flag.StringVar(&path, "path", os.Getenv("GOPATH"), "A path to look for images")
+    flag.StringVar(&imageType, "imageType", "bmp", "The type of image we wish to display")
+    flag.Parse()
+    t := map[string]Gallery{"png": new(PNG), "bmp": new(BMP)}
+    g := t[imageType]
+    g.ShowImages(path)
+}
