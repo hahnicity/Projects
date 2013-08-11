96af89f0501059d53aee958cccc4227337c8d5ed
hahnicity <grehm87@gmail.com>
2013-08-11 03:15:50 -0700

    Refactored downloads.go Refactored the wget implementation

--- Threading/downloads.go
+++ Threading/downloads.go
@@ -17,24 +17,35 @@ const (
     randomURL = "http://archive.ipython.org/release/1.0.0/ipython-1.0.0.zip"
 )
 
-//Main function
-func main() {
+// Make the GET request to a server, return the response
+func getResponse() *http.Response {
     tr := new(http.Transport)
     client := &http.Client{Transport: tr}
     resp, err := client.Get(randomURL)
-    if err != nil {
-        panic(err)    
-    }
+    errorChecker(err)
+    return resp
+}
+
+// Write the response of the GET request to file
+func writeToFile(resp *http.Response) {
+    // Credit for this implementation should go to github user billnapier
     file, err := os.OpenFile("foo", os.O_CREATE|os.O_WRONLY, 0777)
-    if err != nil {
-        panic(err)
-    }
+    defer file.Close()
     bufferedWriter := bufio.NewWriterSize(file, bufSize)
-    if err != nil {
-        panic(err)
-    }
+    errorChecker(err)
     _, err = io.Copy(bufferedWriter, resp.Body)
+    errorChecker(err)
+}
+
+// Check if we received an error on our last function call
+func errorChecker(err error) {
     if err != nil {
         panic(err)
     }
+}
+
+// Main function
+func main() {
+    resp := getResponse()
+    writeToFile(resp)
 }
