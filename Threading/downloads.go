6fec3b40cd3e84056b05d974831d27d4a1d6b4ae
hahnicity <grehm87@gmail.com>
2013-08-20 00:38:41 -0700

    Improved reporting for the download tracker Made sure we are not printing to a new line for each progress update. We now print on only one line during execution. Also cleaned up the code a bit

--- Threading/downloads.go
+++ Threading/downloads.go
@@ -28,20 +28,27 @@ func getResponse() *http.Response {
 
 func monitorFileSize(fileName string, downloadSize, timeout int64) {
     var (
-        elapsed, start, size int64 = 0, 0, 0
+        elapsed, start int64 = 0, 0
+        progress float32 = 0
         ti *time.Time = &time.Time{}
     )
     start = ti.Unix()
     // XXX Only print when at least 1 % more has been added to file
-    for size < downloadSize && elapsed < timeout{
-        file, _ := os.Open(fileName)
-        stats, _ := file.Stat()
-        size := stats.Size()
-        progress := float32(size)/float32(downloadSize) * 100
-        fmt.Println("Your download is %", progress, "percent finished")
+    for progress <= 100.0 && elapsed < timeout{
+        progress = getProgress(fileName, downloadSize)
+        fmt.Printf("\rYour download is %f percent complete",progress)
+        time.Sleep(time.Millisecond * 20)
         elapsed = ti.Unix() - start
-        file.Close()
     }
+}
+
+//Monitor the progress of the download file
+func getProgress(fileName string, downloadSize int64) float32 {
+    file, _ := os.Open(fileName)
+    stats, _ := file.Stat()
+    progress := float32(stats.Size())/float32(downloadSize) * 100
+    file.Close()
+    return progress
 }
 
 // Write the response of the GET request to file
@@ -68,4 +75,5 @@ func main() {
     fileName := "foo"
     go monitorFileSize(fileName, resp.ContentLength, 100)
     writeToFile(fileName, resp)
+    fmt.Print("\n")
 }
