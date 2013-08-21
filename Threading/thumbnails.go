1d5d0095529ce65775bea8cca1f03ba1af0ad8e5
hahnicity <grehm87@gmail.com>
2013-08-21 01:44:22 -0700

    Adding more work done on thumbnails Added more work done on the thumbnails project

--- Threading/thumbnails.go
+++ Threading/thumbnails.go
@@ -2,10 +2,37 @@ package main
 
 import (
     //"github.com/nfnt/resize"
+    "github.com/hahnicity/filetree"
+    "github.com/hahnicity/filterutils"
     "math/rand"
     "fmt"
+    "strings"
 )
-// Can pick this up tomorrow
+
+const (
+    imagePath string = "/home/greg/Pictures"
+)
+
+func getImages(imagePath string) []string {
+   imageTypes := [...]string{".bmp", ".png", ".jpg"}
+   dir, err := filetree.GetDir(imagePath)
+   if err != nil {
+        panic(err)   
+    }
+    files, err := dir.GetFilePaths()
+    if err != nil {
+        panic(err)    
+    }
+    allFiles := make([]string,0)
+    for _, imageType := range imageTypes {
+        filtered := filterutils.Filter(
+            files,
+            func(i int) bool { return strings.HasSuffix(files[i], imageType) },
+        )
+        allFiles = append(allFiles[:len(allFiles)], append(make([]string,0), filtered[:len(filtered)]...)...)
+    }
+    return allFiles
+}
 
 // Pick dimensions that we wish to resize our thumbnail to
 func pickDimensions(bound int32) (uint32, uint32){
