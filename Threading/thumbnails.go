1732371b23dcea4abe8e9483ec1761673de7c3c7
hahnicity <grehm87@gmail.com>
2013-09-03 02:17:53 -0700

    first draft of thumbnail project. Still needs some error handling to go with it. But otherwise it works!

--- Threading/thumbnails.go
+++ Threading/thumbnails.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-    //"github.com/nfnt/resize"
+    "github.com/nfnt/resize"
     "github.com/hahnicity/filetree"
     "github.com/hahnicity/filterutils"
+    "image/jpeg"
+    "log"
     "math/rand"
-    "fmt"
-    "strings"
+    "os"
+    "strconv"
 )
+
+type Dimensions struct {
+    height, width uint
+}
 
 const (
-    imagePath string = "/home/greg/Pictures"
+    FindImagePath string = "/home/greg/Pictures/"
+    PutImagePath string = "/home/greg/Pictures/"
 )
 
+// XXX Arrays arent immutable by nature so you cant make them constant
+var Choices = []*Dimensions{
+    &Dimensions{75,75}, 
+    &Dimensions{125,125}, 
+    &Dimensions{150,150},
+    &Dimensions{200,200},
+}
+
+// Get a list of all supported image files in a directory
 func getImages(imagePath string) []string {
-   imageTypes := [...]string{".bmp", ".png", ".jpg"}
+    // Currently only supports .jpg's
+   imageTypes := [...]string{".jpg"}
    dir, err := filetree.GetDir(imagePath)
    if err != nil {
         panic(err)   
@@ -23,25 +40,72 @@ func getImages(imagePath string) []string {
     if err != nil {
         panic(err)    
     }
-    allFiles := make([]string,0)
-    for _, imageType := range imageTypes {
-        filtered := filterutils.Filter(
-            files,
-            func(i int) bool { return strings.HasSuffix(files[i], imageType) },
-        )
-        allFiles = append(allFiles[:len(allFiles)], append(make([]string,0), filtered[:len(filtered)]...)...)
-    }
-    return allFiles
+    return filterutils.HasSuffix(files, imageTypes[0])
+
+    // XXX Commented code is not a good practice, but this was good learning material
+    //allFiles := make([]string,0)
+    //for _, imageType := range imageTypes {
+    //    filtered := filterutils.HasSuffix(files, imageType)
+    //    allFiles = append(
+    //        allFiles[:len(allFiles)], 
+    //        append(
+    //            make([]string,0), 
+    //            filtered[:len(filtered)]...
+    //        )...
+    //    )
+    //}
 }
 
 // Pick dimensions that we wish to resize our thumbnail to
-func pickDimensions(bound int32) (uint32, uint32){
-    var (
-        height, width uint32 = uint32(rand.Int31n(bound) + 1), uint32(rand.Int31n(bound) + 1)
-    )
-    return height, width
+func pickDimensions() *Dimensions {
+    n := int32(len(Choices))
+    return Choices[rand.Int31n(n)]
+}
+
+// Resize all images given to it
+func resizeImages(path string, dim *Dimensions, progress chan int) {
+    file, err := os.Open(path)
+    if err != nil {
+        log.Fatal(err)        
+        return
+    }
+    img, err := jpeg.Decode(file)
+    file.Close()
+    if err != nil {
+        log.Fatal(err)
+        return   
+    }
+    res := resize.Resize(dim.height, dim.width, img, resize.Lanczos3)
+    out, err := os.Create(PutImagePath + strconv.FormatInt(rand.Int63(),36) + ".jpg")
+    if err != nil {
+        log.Fatal(err)    
+    }
+    defer out.Close()
+    jpeg.Encode(out, res, nil)
+    progress <- 1
+}
+
+// Monitor the overall progress of the file conversion
+func monitorProgress(progress chan int, n int) {
+    totalFiles := 0
+    for received := range progress {
+        totalFiles = totalFiles + received
+        // XXX What happens if theres an error? This file will loop infinitely
+        if totalFiles >= n {
+            close(progress)    
+        }
+    }
+    
 }
 
 func main() {
-    fmt.Println(rand.Uint32())
+    // XXX Move path to argument parser
+    images := getImages(FindImagePath)
+    // Implements a channel. They are simply conveyer belts.
+    progress := make(chan int)
+    for _, path := range(images) {
+        // Every file gets its own thread! YAY!
+        go resizeImages(path, pickDimensions(), progress)
+    }
+    monitorProgress(progress, len(images))
 }
